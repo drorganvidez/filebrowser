38e50af89fecf84a12421623490bda569b0d6b88
David Romero Organvídez <drorganvidez@icloud.com>
2024-01-18 00:12:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Solicita el valor del subdominio y almacénalo en la variable "server"
-read -p "Ingrese el nombre del subdominio X para '/filebrowser/X: " server
-read -p "Ingrese el usuario del sistema: " user
+read -p "Ingrese el nombre del subdominio X para '/filebrowser/X': " server
+read -p "Ingrese el nombre de usuario del sistema donde se está instalando File Browser: " user
+read -p "Ingrese un usuario administrador de File Browser: " filebrowser_user
+read -p "Ingrese la contraseña del usuario administrador de File Browser: " filebrowser_pass
 
 # Actualiza los paquetes del sistema
 sudo apt update
@@ -10,6 +12,27 @@ sudo apt upgrade -y
 
 # Instala File Browser
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+
+# Crea la configuración de File Browser
+sudo mkdir -p /etc/filebrowser
+sudo tee /etc/filebrowser/default.json > /dev/null <<EOL
+{
+  "port": 4201,
+  "baseURL": "/filebrowser/$server",
+  "address": "",
+  "log": "stdout",
+  "database": "/etc/filebrowser/filebrowser.db",
+  "root": "/",
+  "auth": true
+}
+EOL
+
+# Crea el archivo filebrowser.db
+sudo /usr/local/bin/filebrowser -d /etc/filebrowser/filebrowser.db config init
+
+# Agrega el usuario proporcionado con la contraseña proporcionada
+cd /etc/filebrowser
+sudo /usr/local/bin/filebrowser users add $filebrowser_user "$filebrowser_pass" --perm.admin
 
 # Crea el archivo de configuración systemd
 sudo tee /etc/systemd/system/filebrowser.service > /dev/null <<EOL
@@ -26,23 +49,7 @@ ExecStart=/usr/local/bin/filebrowser -c /etc/filebrowser/default.json
 WantedBy=multi-user.target
 EOL
 
-# Crea la configuración de File Browser
-sudo mkdir -p /etc/filebrowser
-sudo tee /etc/filebrowser/default.json > /dev/null <<EOL
-{
-  "port": 4201,
-  "baseURL": "/filebrowser/$server",
-  "address": "",
-  "log": "stdout",
-  "database": "/etc/filebrowser/filebrowser.db",
-  "root": "/"
-}
-EOL
-
-# Mueve el archivo filebrowser.db a la ubicación adecuada
-sudo mv ~/filebrowser.db /etc/filebrowser
-
-# Configura los permisos correctos para la base de datos
+# Configura los permisos correctos para el usuario en la base de datos
 sudo chown -R $user:$user /etc/filebrowser/filebrowser.db
 
 # Habilita File Browser para que inicie en el arranque del sistema
@@ -56,3 +63,5 @@ sudo service filebrowser status
 
 echo "File Browser ha sido instalado y configurado en el puerto 4201."
 echo "Puedes acceder a File Browser en http://tu-direccion-ip:4201/filebrowser/$server"
+echo "Usuario: $filebrowser_user"
+echo "Contraseña: $filebrowser_pass"
